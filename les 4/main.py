a5c5bea61bdcfe5274bec95adc69a5f1ba247278
Nikita <nikitadeduskin45@gmail.com>
2023-05-14 15:33:26 +0300

    #les 3

---
+++ les 4/main.py
@@ -0,0 +1,3 @@
+from student import ItStepStudent
+kyrylo = ItStepStudent("Kyrylo", 11, 160, "V2011", 12)
+print(kyrylo.__str__())
---
+++ les 4/speak.py
@@ -0,0 +1,3 @@
+class Speak:
+    def SayName(self, name):
+        print("My name is Kyrylo")
